f61300c03414a625d13e38a305509a78d176999a
gon <gon.galili5@gmail.com>
2017-11-24 09:46:52 +0200

    20.11

--- app/src/main/java/com/example/magshimim/ex7/MainActivity.java
+++ app/src/main/java/com/example/magshimim/ex7/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.magshimim.ex7;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +12,29 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void sendMessage(View view)
+    {
+        Intent intent = new Intent(this,RegisterActivity.class);
+        startActivityForResult(intent,1);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+            if (resultCode == RESULT_OK) { // Activity.RESULT_OK
+
+                // get String data from Intent
+                String returnString = data.getStringExtra("firstName");
+                String returnString2 = data.getStringExtra("lastName");
+
+                // set text view with string
+                TextView textView = (TextView) findViewById(R.id.intro);
+
+                textView.setText(returnString);
+            }
+
     }
 }
--- app/src/main/java/com/example/magshimim/ex7/RegisterActivity.java
+++ app/src/main/java/com/example/magshimim/ex7/RegisterActivity.java
@@ -1,7 +1,15 @@
 package com.example.magshimim.ex7;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class RegisterActivity extends Activity {
 
@@ -9,5 +17,23 @@ public class RegisterActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
     }
+
+    public void onButtonClick(View view) {
+
+        // get the text from the EditText
+        EditText firstName = (EditText) findViewById(R.id.inF);
+        EditText lastName = (EditText) findViewById(R.id.inL);
+        
+        String stringToPassBack = firstName.getText().toString();
+        String stringToPassBack2 = lastName.getText().toString();
+        // put the String to pass back into an Intent and close this activity
+        Intent intent = new Intent();
+        intent.putExtra("firstName", stringToPassBack);
+        intent.putExtra("lastName", stringToPassBack2);
+        setResult(RESULT_OK, intent);
+        finish();
+    }
+
 }
